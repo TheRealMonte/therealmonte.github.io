8b3cff59231d086fc78dc2910a8b4d71f7b8d492
TheRealMonte <ryanamontville@gmail.com>
2024-07-19 18:08:00 -0400

    uploading pixels csv

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DrawComponent } from './draw/draw.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'draw', component: DrawComponent}
 ];
