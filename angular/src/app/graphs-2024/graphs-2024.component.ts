e0b63365091dd87a95f9c4b047dd35e1c3affd17
TheRealMonte <ryanamontville@gmail.com>
2024-08-08 23:38:10 -0400

    adding footer to graphs page

--- angular/src/app/graphs-2024/graphs-2024.component.ts
+++ angular/src/app/graphs-2024/graphs-2024.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-graphs-2024',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './graphs-2024.component.html',
   styleUrl: './graphs-2024.component.css'
 })
